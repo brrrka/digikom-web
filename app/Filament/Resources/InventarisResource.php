32e814cbd490b388300c1e60fc045c6d18c90012
FajarRp11 <fajarrahyu1103@gmail.com>
2025-01-14 12:31:53 +0700

    inventaris

--- app/Filament/Resources/InventarisResource.php
+++ app/Filament/Resources/InventarisResource.php
@@ -83,7 +83,9 @@ class InventarisResource extends Resource
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
