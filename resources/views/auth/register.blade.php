472391e8ccd609532686f2b037b18f654523e78e
brrrka <berkaaldizar8@gmail.com>
2025-01-31 15:55:29 +0700

    Register Page Tampilan only

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,77 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<x-app-layout>
+    <div class="py-20 -mb-20"> <!-- Tambahkan padding atas bawah untuk jarak dari navbar dan footer -->
+        <div
+            class="min-h-[calc(100vh-160px)] flex items-center justify-center bg-gradient-to-b from-white via-white to-[#ffffec] relative overflow-hidden">
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+            <div class="absolute right-0 top-40">
+                <img src="{{ asset('images/Group11.png') }}" />
+            </div>
+            <div class="absolute right-[286px] top-[86px]">
+                <img src="{{ asset('images/Rectangle7.png') }}" />
+            </div>
+            <div class="absolute right-72 top-24">
+                <img src="{{ asset('images/Rectangle8.png') }}" />
+            </div>
+            <div class="absolute left-[350px] top-72">
+                <img src="{{ asset('images/Group12.png') }}" />
+            </div>
+            <div class="absolute left-0 top-40">
+                <img src="{{ asset('images/Group2.png') }}" />
+            </div>
+            <div class="absolute left-20 top-48">
+                <img src="{{ asset('images/Ellipse62.png') }}" />
+            </div>
+
+            <div class="bg-white py-8 px-24 rounded-3xl shadow-2xl w-full max-w-xl mx-4 relative my-8">
+                <h2 class="text-2xl font-semibold text-dark-green-2 mb-2 text-center">Daftar</h2>
+
+                <form method="POST" action="{{ route('login') }}" class="space-y-8">
+                    @csrf
+
+                    <div class="mb-6 relative">
+                        <label for="email"
+                            class="absolute -top-3 text-sm text-dark-green ml-8 bg-white px-1">Email</label>
+                        <input type="email" id="email" name="email"
+                            class="w-full px-8 py-3 rounded-3xl border-2 border-primary focus:border-primary focus:ring-2 focus:ring-primary transition-colors placeholder-[#FBF8B5]"
+                            required placeholder="Ketik Email">
+                    </div>
+
+                    <div class="mb-6 relative">
+                        <label for="username"
+                            class="absolute -top-3 text-sm text-dark-green ml-8 bg-white px-1">Username</label>
+                        <input type="text" id="Username" name="Username"
+                            class="w-full px-8 py-3 rounded-3xl border-2 border-primary focus:border-primary focus:ring-2 focus:ring-primary transition-colors placeholder-[#FBF8B5]"
+                            required placeholder="Ketik Username">
+                    </div>
+
+                    <div class="mb-6 relative">
+                        <label for="password"
+                            class="absolute -top-3 text-sm text-dark-green ml-8 bg-white px-1">Password</label>
+                        <input type="password" id="password" name="password"
+                            class="w-full px-8 py-3 rounded-3xl border-2 border-primary focus:border-primary focus:ring-2 focus:ring-primary transition-colors placeholder-[#FBF8B5]"
+                            required placeholder="Ketik Password">
+                    </div>
+
+                    <div class="mb-6 relative">
+                        <label for="password_confirmation"
+                            class="absolute -top-3 text-sm text-dark-green ml-8 bg-white px-1">Confirm
+                            Password</label>
+                        <input type="password" id="password_confirmation" name="password_confirmation"
+                            class="w-full px-8 py-3 rounded-3xl border-2 border-primary focus:border-primary focus:ring-2 focus:ring-primary transition-colors placeholder-[#FBF8B5]"
+                            required placeholder="Ketik Password">
+                    </div>
+
+                    <button type="submit"
+                        class="w-full py-4 px-4 bg-dark-green-2 hover:bg-[#5d745a] text-white rounded-3xl transition-colors mt-8">
+                        Daftar
+                    </button>
+
+                    <div class="text-center text-sm text-dark-green-2 mt-6">
+                        Sudah memiliki akun?
+                        <a href="{{ route('login') }}" class="text-[#718B6D] hover:underline font-semibold">Masuk</a>
+                    </div>
+                </form>
+            </div>
         </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+</x-app-layout>
