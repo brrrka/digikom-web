bb1682e3aaa4304dde38a8720bcc706b0e892052
brrrka <berkaaldizar8@gmail.com>
2025-01-31 15:33:23 +0700

    Login Page Tampilan only

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,59 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<x-app-layout>
+    <div
+        class="min-h-screen flex items-center justify-center bg-gradient-to-b from-white via-white to-[#eef8e2] relative overflow-hidden">
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div class="absolute right-0 top-40">
+            <img src="{{ asset('images/Group11.png') }}" />
+        </div>
+        <div class="absolute right-[286px] top-[86px]">
+            <img src="{{ asset('images/Rectangle7.png') }}" />
+        </div>
+        <div class="absolute right-72 top-24">
+            <img src="{{ asset('images/Rectangle8.png') }}" />
+        </div>
+        <div class="absolute left-[350px] top-72">
+            <img src="{{ asset('images/Group12.png') }}" />
+        </div>
+        <div class="absolute left-0 top-40">
+            <img src="{{ asset('images/Group2.png') }}" />
+        </div>
+        <div class="absolute left-20 top-48">
+            <img src="{{ asset('images/Ellipse62.png') }}" />
         </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+        <div class="bg-white py-16 px-24 rounded-3xl shadow-2xl w-full max-w-xl mx-4 relative">
+            <h2 class="text-2xl font-semibold text-dark-green-2 mb-16 text-center">Masuk</h2>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <form method="POST" action="{{ route('login') }}" class="space-y-12">
+                @csrf
+
+                <div class="mb-12 relative">
+                    <label for="email"
+                        class="absolute -top-3 text-sm text-dark-green ml-8 bg-white px-1">Email</label>
+                    <input type="email" id="email" name="email"
+                        class="w-full px-8 py-3 rounded-3xl border-2 border-primary focus:border-primary focus:ring-2 focus:ring-primary transition-colors placeholder-[#FBF8B5]"
+                        required placeholder="Ketik Email">
+                </div>
+
+                <div class="mb-12 relative">
+                    <label for="password"
+                        class="absolute -top-3 text-sm text-dark-green ml-8 bg-white px-1">Password</label>
+                    <input type="password" id="password" name="password"
+                        class="w-full px-8 py-3 rounded-3xl border-2 border-primary focus:border-primary focus:ring-2 focus:ring-primary transition-colors placeholder-[#FBF8B5]"
+                        required placeholder="Ketik Password">
+                </div>
+
+                <button type="submit"
+                    class="w-full py-4 px-4 bg-dark-green-2 hover:bg-[#5d745a] text-white rounded-3xl transition-colors mt-12">
+                    Masuk
+                </button>
+
+                <div class="text-center text-sm text-dark-green-2 mt-8">
+                    Belum memiliki akun?
+                    <a href="{{ route('register') }}" class="text-[#718B6D] hover:underline font-semibold">Daftar</a>
+                </div>
+            </form>
         </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </div>
+</x-app-layout>
